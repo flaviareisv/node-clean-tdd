59976b09ddeae139f41ed151feb826f0d165e043
Flavia Reis Valverde <fareisv@gmail.com>
2022-06-06 18:30:27 -0300

    feat: ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,16 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter() // utiliza o sut (system under test) pois é o padrão para dizer que está sendo testado
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
